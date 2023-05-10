44eb3e778ffaf3cb4b549ff2d565ea173e586fe4
Dmitry <Diman_krasnov02@mail.ru>
2023-05-10 05:40:40 +0300

    some upd

--- ZLPLibrary/View/AddBookPage.xaml.cs
+++ ZLPLibrary/View/AddBookPage.xaml.cs
@@ -1,16 +1,32 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using Xamarin.Forms;
+using ZLPLibrary.Model;
 using ZLPLibrary.ViewModel;
 
 namespace ZLPLibrary.View
 {
     public partial class AddBookPage : ContentPage
 	{
-		public AddBookPage ()
+        public FullBook fullBook;
+        public AddBookPage ()
 		{
 			InitializeComponent ();
             NavigationPage.SetHasNavigationBar(this, false);
-            BindingContext = new AddBookPageViewModel();
+            BindingContext = new AddBookPageViewModel(fullBook);
         }
-	}
+        async void OnTappedToBack(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private void Completed(object sender, EventArgs e)
+        {
+            fullBook.bookName = bookNameEntry.Text;
+            fullBook.authors = authorsEntry.Text;
+            fullBook.numberOfPage = Convert.ToInt32(numberOfPageEntry.Text);
+            fullBook.publishingHouse = publishingHouseEntry.Text;
+            fullBook.yearOfPublication = yearOfPublishingEntry.Text;
+        }
+    }
 }
--- ZLPLibrary/ViewModel/AddBookPageViewModel.cs
+++ ZLPLibrary/ViewModel/AddBookPageViewModel.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using ZLPLibrary.Model;
 
 namespace ZLPLibrary.ViewModel
 {
-    public class AddBookPageViewModel
+    public class AddBookPageViewModel : INotifyPropertyChanged
     {
-        public AddBookPageViewModel()
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand AddNewBookCommand { protected get; set; }
+        public AddBookPageViewModel(FullBook fullBook)
         {
-            
+            AddNewBookCommand = new Command();//разберись димон дальше
+        }
+        private void AddNewBook(FullBook fullBook)
+        {
+
         }
     }
 }
